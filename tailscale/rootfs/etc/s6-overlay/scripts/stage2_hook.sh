4bd1042d24dabb36b61386a508428d9e4baa3002
Laszlo Magyar <lmagyar1973@gmail.com>
2026-01-11 14:19:05 +0100

    rename tags to advertise_tags (config is updated automatically)

--- tailscale/rootfs/etc/s6-overlay/scripts/stage2_hook.sh
+++ tailscale/rootfs/etc/s6-overlay/scripts/stage2_hook.sh
@@ -9,6 +9,7 @@ export LOG_FD
 declare options
 declare proxy funnel proxy_and_funnel_port
 declare share_service_name
+declare tags
 
 # This is to execute potentially failing supervisor api functions within conditions,
 # where set -e is not propagated inside the function and bashio relies on set -e for api error handling
@@ -58,6 +59,14 @@ fi
 if bashio::var.has_value "${proxy_and_funnel_port}"; then
     bashio::log.info 'Removing deprecated proxy_and_funnel_port option'
     bashio::addon.option 'proxy_and_funnel_port'
+fi
+
+# Rename changed options
+tags=$(bashio::jq "${options}" '.tags | select(.!=null)')
+if bashio::var.has_value "${tags}"; then
+    bashio::log.info 'Renaming tags option to advertise_tags'
+    bashio::addon.option 'tags'
+    bashio::addon.option 'advertise_tags' "^${tags}"
 fi
 
 # Remove deprecated share_service_name option
